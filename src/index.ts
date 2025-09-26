9d081524f5c83b26520685651e241b7f0a4de412
AyushChamoli961 <ayushchamoli001@gmail.com>
2025-09-26 13:08:27 +0530

    cors url udpated

--- src/index.ts
+++ src/index.ts
@@ -41,7 +41,11 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use(
   cors({
-    origin: ["http://localhost:3000", "http://localhost:3001"],
+    origin: [
+      "http://localhost:3000",
+      "http://localhost:3001",
+      "https://lms-main-red.vercel.app",
+    ],
     credentials: true,
     methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
   })
